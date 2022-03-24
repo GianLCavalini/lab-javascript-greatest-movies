27d3f82356099cad142887f4a5ca2a76e521c31e
Gian Lucca <giandacoll@gmail.com>
2022-03-24 18:52:10 -0300

    iterações 1, 2 e 3 prontas

--- src/movies.js
+++ src/movies.js
@@ -1,17 +1,74 @@
+const movies = require("./data");
+
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
+//console.log('movies: ', movies);
 
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  
+  const mappedArrays = movies.map((currentElement) => currentElement.director);// tudo o que estiver depois da seta vai ser o return
+  return mappedArrays;
+ // console.log(mappedArrays);
+  
+}
+ getAllDirectors(movies);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+
+
+ // const stevenMovies = movies.filter((currentElement) => currentElement.director === 'Steven Spielberg' && currentElement.genre.indexOf('Drama') === 'Drama'); 
+  const stevenMovies = movies.filter((currentElement) => {
+    return(currentElement.director === 'Steven Spielberg' && currentElement.genre.includes('Drama') )
+  })  
+
+  return stevenMovies.length;
+
+}
+
+howManyMovies(movies);
+
+
+
+
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if(movies.length === 0){
+    return 0;
+  }
+
+  const newScore = movies.map((currentElement) => currentElement.score);
+  console.log(newScore);
+
+  const filteredScore = newScore.filter((currentElement) => { if(typeof currentElement === 'number')
+  return currentElement})
+
+  const filterReduc = filteredScore.reduce((a,b) => a+b)
+  //return filterReduc;
+
+  const scoreMedia = filterReduc / newScore.length;
+  return Number (scoreMedia.toFixed(2));
+
+   //console.log(filteredScore)
+  
+
+}
+
+//scoresAverage(movies);
+
+
+
+
+
+
+
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
